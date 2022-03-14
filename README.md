907933070efd846d99232e3d1a68ef8dd37ccec4
FC\abm <34012094+fireflycons@users.noreply.github.com>
2022-03-14 20:20:29 +0000

    Add decrypt of cipher string from cmd line Also imporve readme.

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
-You will need the following files from within the Jenkins home dir
+# Jenkins Password Decryptor
 
-* master.key
-* hudson.util.Secret
-* credentials.xml
+You will need the following files from within the Jenkins home directory:
 
-Run the script:
+* `master.key`
+* `hudson.util.Secret`
+* `credentials.xml` or a secret cipehrtext which looks like base64 surrounded with `{}`
+
+## Decrypt secrets in credentials.xml
+
+This will decrypyt secrets of the following types
+
+* `com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImp`
+* `com.cloudbees.plugins.credentials.impl.StringCredentialsImpl`
+* `com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey`
+* `com.dabsquared.gitlabjenkins.connection.GitLabApiTokenImpl`
+* `com.datapipe.jenkins.vault.credentials.VaultAppRoleCredential`
+
+Others can easily be added if you add the appropriate code to extract the secret type from the XML.
+
 ```
-./decrypt.py ./master.key ./hudson.util.Secret ./credentials.xml
+./decrypt.py path/to/master.key path/to/hudson.util.Secret path/to/credentials.xml
+```
+
+## Decrypt a single ciphertext
+
+```
+./decrypt.py path/to/master.key path/to/hudson.util.Secret '{ciphertext}'
 ```
--- decrypt.py
+++ decrypt.py
@@ -69,48 +69,55 @@ def main():
   secret = secret[:-16]
   secret = secret[:16]
 
-  credentials = open(sys.argv[3]).read()
-  xml = ET.parse(sys.argv[3])
-
   password_entries = []
-  for elem in xml._root.iter('com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl'):
-    password_entries.append ({
-      'Type': 'UsernamePasswordCredentials',
-      'Id': elem.find('id').text,
-      'Username': elem.find('username').text,
-      'CipherText': elem.find('password').text
-    })
 
-  for elem in xml._root.iter('com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey'):
-    password_entries.append ({
-      'Type': 'BasicSSHUserPrivateKey',
-      'Id': elem.find('id').text,
-      'Username': elem.find('username').text,
-      'CipherText': elem.find('privateKeySource').find('privateKey').text
+  if (sys.argv[3].startswith('{')):
+    password_entries.append({
+      'Type': 'CommandLine',
+      'Id': 'CommandLine',
+      'CipherText': sys.argv[3]
     })
+  else:
+    xml = ET.parse(sys.argv[3])
 
-  for elem in xml._root.iter('com.cloudbees.plugins.credentials.impl.StringCredentialsImpl'):
-    password_entries.append ({
-      'Type': 'StringCredentials',
-      'Id': elem.find('id').text,
-      'CipherText': elem.find('secret').text
-    })
+    for elem in xml._root.iter('com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl'):
+      password_entries.append ({
+        'Type': 'UsernamePasswordCredentials',
+        'Id': elem.find('id').text,
+        'Username': elem.find('username').text,
+        'CipherText': elem.find('password').text
+      })
 
-  for elem in xml._root.iter('com.dabsquared.gitlabjenkins.connection.GitLabApiTokenImpl'):
-    password_entries.append ({
-      'Type': 'GitLabApiToken',
-      'Id': elem.find('id').text,
-      'CipherText': elem.find('apiToken').text
-    })
+    for elem in xml._root.iter('com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey'):
+      password_entries.append ({
+        'Type': 'BasicSSHUserPrivateKey',
+        'Id': elem.find('id').text,
+        'Username': elem.find('username').text,
+        'CipherText': elem.find('privateKeySource').find('privateKey').text
+      })
 
-  for elem in xml._root.iter('com.datapipe.jenkins.vault.credentials.VaultAppRoleCredential'):
-    password_entries.append ({
-      'Type': 'VaultAppRoleCredential',
-      'Id': elem.find('id').text,
-      'CipherText': elem.find('secretId').text,
-      'RoleId': elem.find('roleId').text,
-      'Path': elem.find('path').text
-    })
+    for elem in xml._root.iter('com.cloudbees.plugins.credentials.impl.StringCredentialsImpl'):
+      password_entries.append ({
+        'Type': 'StringCredentials',
+        'Id': elem.find('id').text,
+        'CipherText': elem.find('secret').text
+      })
+
+    for elem in xml._root.iter('com.dabsquared.gitlabjenkins.connection.GitLabApiTokenImpl'):
+      password_entries.append ({
+        'Type': 'GitLabApiToken',
+        'Id': elem.find('id').text,
+        'CipherText': elem.find('apiToken').text
+      })
+
+    for elem in xml._root.iter('com.datapipe.jenkins.vault.credentials.VaultAppRoleCredential'):
+      password_entries.append ({
+        'Type': 'VaultAppRoleCredential',
+        'Id': elem.find('id').text,
+        'CipherText': elem.find('secretId').text,
+        'RoleId': elem.find('roleId').text,
+        'Path': elem.find('path').text
+      })
 
   # You can find the password format at https://github.com/jenkinsci/jenkins/blob/master/core/src/main/java/hudson/util/Secret.java#L167-L216
 
@@ -126,7 +133,7 @@ def main():
 
     print (password_entry['Type'])
     print ('-' * len(password_entry['Type']))
-    print(f'Id:       {password_entry["Id"]}')
+    print(f'Id: {password_entry["Id"]}')
 
     if password_entry['Type'] == 'UsernamePasswordCredentials':
       print(f'Username: {password_entry["Username"]}')
@@ -135,7 +142,7 @@ def main():
       print(f'Username: {password_entry["Username"]}')
       print('Private Key:')
       print(password)
-    elif password_entry['Type'] == 'StringCredentials':
+    elif password_entry['Type'] in ['StringCredentials', 'CommandLine']:
       print(f'Password: {password}')
     elif password_entry['Type'] == 'GitLabApiToken':
       print(f'Token: {password}')
